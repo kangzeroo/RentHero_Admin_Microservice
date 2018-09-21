1fc26326ab2d53f906a8c22ce925e52e0b347f90
Jimmy Guo <jimmyzidanguo@hotmail.com>
2018-09-21 17:07:19 -0400

    asd

--- routes/corp_routes.js
+++ routes/corp_routes.js
@@ -11,3 +11,16 @@ exports.get_all_corporations = (req, res, next) => {
       res.status(500).send(err)
     })
 }
+
+exports.save_staff_to_corporation = (req, res, next) => {
+  const info = req.body
+
+  CorpQueries.save_staff_to_corporation(info.corporation_id, info.staff_email)
+    .then((data) => {
+      res.json(data)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).send(err)
+    })
+}
